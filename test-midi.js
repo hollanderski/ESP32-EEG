a15761c52d17d813964c5fd979b1cae6dd3d6e2e
hollanderski <lizeninon@yahoo.fr>
2023-02-07 18:10:23 +0100

    CC message interval

--- test-midi.js
+++ test-midi.js
@@ -1,5 +1,6 @@
+let ports = ['Meditation MIDI', 'BetaBusy MIDI', 'AlphaRest MIDI', 'loopMIDI Port'];
 var easymidi = require('easymidi');
-var output = new easymidi.Output('loopMIDI Port');
+var output = new easymidi.Output(ports[0]); 
 
 //noteoff
 
@@ -13,7 +14,35 @@ output.send('noteon', {
 
 // CC message
 output.send('cc', {
-  controller: 11,
-  value: 127,
+  controller: 1,
+  value: 27,
   channel: 3
 });
+
+
+
+
+let sendCC = () => {
+
+    output.send('cc', {
+    controller: 1,
+    value: 120,//127,
+    channel: 3
+  });
+
+
+    setTimeout(() => {
+
+        output.send('cc', {
+        controller: 1,
+        value: 0,
+        channel: 3
+      });
+
+    }, 300);
+}
+
+
+setInterval(() => {
+  sendCC()
+}, 1000);
